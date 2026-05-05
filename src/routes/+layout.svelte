06e547717a0afbc9bcfc414b0e6acf3d0fc266ad
Henrique-RMotta <henrique2008790@gmail.com>
2026-05-05 15:02:33 -0300

    feat: add chart.js and svelte-chartjs dependencies; update layout and page structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import Sidebar from '$lib/components/layout/Sidebar.svelte';
+	import Topbar from '$lib/components/layout/Topbar.svelte';
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="flex flex-col">
+	<Topbar/>
+	<div class="flex flex-row">
+		<Sidebar/>
+		{@render children()}
+	</div>
+</div>
